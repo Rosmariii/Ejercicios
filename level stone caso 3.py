32be7c528ecf82ff13848ccd620141ced5650190
Rosmari <rosmarifernandez@hotmail.com>
2021-02-05 12:02:10 -0300

    lo hiceee

--- level stone caso 3.py
+++ level stone caso 3.py
@@ -1,18 +1,42 @@
-class Triangulo():
+class Triángulo():
     def __init__(self, ladoA, ladoB, ladoC):
             self.ladoA = ladoA
             self.ladoB = ladoB
             self.ladoC = ladoC
 
     def LadoLargo(self):
-        lados
-        if self.ladoA > self.ladoB & self.ladoC:
+        if self.ladoA > self.ladoB and self.ladoA > self.ladoC:
             print(f'El lado A es el de mayor tamaño {self.ladoA}')
-        elif self.ladoB > self.ladoA & self.ladoC:
+        elif self.ladoB > self.ladoA and self.ladoB > self.ladoC:
             print(f'el lado B es el de mayor tamaño {self.ladoB}')
         else:
             print(f'El lado C es el de mayor tamaño {self.ladoC}')
 
-unTriangulo = Triangulo()
-dffgfsgfdgdf
+    def TipoTriángulo(self):
+        if self.ladoA == self.ladoB & self.ladoB == self.ladoC:
+            print('Este triángulo es de tipo Équilatero')
+        elif self.ladoA == self.ladoB or self.ladoB == self.ladoC or self.ladoA == self.ladoC:
+            print('Este triángulo es de tipo Isóseles')
+        else: 
+            print('Este triángulo es de tipo Escaleno') 
 
+     
+
+while(True):
+    try: 
+        lA = (int(input('Ingrese el tamaño del lado A del triangulo: ')))
+        lB = (int(input('Ingrese el tamaño del lado B del triangulo: ')))
+        lC = (int(input('Ingrese el tamaño del lado C del triangulo: ')))
+        break
+    except:
+        print("Ha ocurrido un error, introduce bien el número entero")
+
+unTriángulo = Triángulo(lA,lB,lC)
+
+print(unTriángulo.LadoLargo())
+print(unTriángulo.TipoTriángulo())
+
+
+
+#git add .
+#git commit -m ""
